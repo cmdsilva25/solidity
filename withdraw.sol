1b85f961a0b0e6b71334f0525e6f732cf904035b
cmdsilva25 <83644309+cmdsilva25@users.noreply.github.com>
2025-01-06 10:58:42 -0300

    Create withdraw.sol

---
+++ withdraw.sol
@@ -0,0 +1,8 @@
+  function withdraw(uint256 _amount) public {
+      require(balances[msg.sender] >= _amount, "Insufficient balance");
+
+      (bool success, ) = msg.sender.call{value: _amount}("");
+      require(success, "Transfer failed");
+
+      balances[msg.sender] -= _amount;
+  }
